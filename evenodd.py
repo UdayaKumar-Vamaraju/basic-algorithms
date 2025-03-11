b5c7ba43a20cf07f8c4ecb0831f505ce6d35a6cb
Udaya Kumar Vamaraju <122080642+UdayaKumar-Vamaraju@users.noreply.github.com>
2025-03-11 17:22:28 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number:"))
+if n<1:
+    print("Enter a positive number")
+else:
+    if n%2==0:
+        print("n is even")
+    else:
+        print("n is odd")
+
---
+++ prime.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number:"))
+
+if n<2:
+    print("Not a prime")
+else:
+    for i in range(2,n):
+        if(n%i==0):
+            print("Not a prime")
+            break
+    else:
+         print("It is a prime")
+    
